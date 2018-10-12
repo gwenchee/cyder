3a2337098059f8e6634f373e2d44a0b8837e9eff
gwenchee <gwendolynchee95@gmail.com>
2018-10-12 12:19:50 -0500

    storage tests --> conditioning words

---
+++ src/conditioning_tests.h
@@ -0,0 +1,40 @@
+#ifndef CONDITIONING_TESTS_H_
+#define CONDITIONING_TESTS_H_
+
+#include <gtest/gtest.h>
+
+#include "conditioning.h"
+
+#include "context.h"
+#include "facility_tests.h"
+#include "agent_tests.h"
+
+namespace conditioning {
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+class ConditioningTest : public ::testing::Test {
+ protected:
+  cyclus::TestContext tc_;
+  Conditioning* src_facility_;
+
+  virtual void SetUp();
+  virtual void TearDown();
+  void InitParameters();
+  void SetUpConditioning();
+  void TestInitState(conditioning::Conditioning* fac);
+  void TestAddMat(conditioning::Conditioning* fac, 
+      cyclus::Material::Ptr mat);
+  void TestBuffers(conditioning::Conditioning* fac, double inv, double 
+      proc, double ready, double stocks);
+  void TestStocks(conditioning::Conditioning* fac, cyclus::CompMap v);
+  void TestReadyTime(conditioning::Conditioning* fac, int t);
+  void TestCurrentCap(conditioning::Conditioning* fac, double inv);
+
+  std::vector<std::string> in_c1, out_c1;
+  std::string in_r1;
+
+  int residence_time;
+  double throughput, max_inv_size;
+  bool discrete_handling;
+};
+} // namespace conditioning
+#endif // CONDITIONING_TESTS_H_
